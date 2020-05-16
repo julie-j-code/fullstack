036c5d0144709272c328d67fab2d6b8e1e02f4da
julie-j-code <jeannet.julie@gmail.com>
2020-05-16 12:18:16 +0200

    creation tokens authentification

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+// Pour pouvoir créer et vérifier les tokens d'authentification, 
+// nous avons ajouté le package jsonwebtoken que nous importons ici
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -29,6 +32,30 @@ exports.login = (req, res, next) => {
                     res.status(200).json({
                         userId: user._id,
                         token: 'TOKEN'
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
+                    // nous utilisons la fonction sign dejsonwebtoken pour encoder un nouveau token
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign({ userId: user._id },
+                            'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }
+                        )
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
